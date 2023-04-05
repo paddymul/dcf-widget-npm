089a892da0bfe6a24ebbb7dae32154fa184220c4
Paddy Mullen <paddy@paddymullen.com>
2023-04-05 15:22:07 -0400

    removed a large number of old react-edit-list fiels

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,3 @@
-import ReactEditList from './EditList';
-
-export {Row, Schema, Props, Element} from './base';
-export {default as ReactEditList} from './EditList';
-
 export {DCFCell, WidgetDCFCell, CommandConfigSetterT} from './components/DCFCell';
 export {CommandConfigT, Command} from './components/CommandUtils';
 export {DFWhole} from './components/staticData';
